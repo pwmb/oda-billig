ce935106edde046a31c93cd0a6d270c85baa1e4b
Soumesh Banerjee <soumeshbanerjee@live.com>
2021-02-02 15:21:43 +0530

    Add m as unit

--- content.js
+++ content.js
@@ -2,19 +2,19 @@ const unitPrices = Array.from(document.querySelectorAll(".unit-price"));
 
 let cheapestPrice = Number.MAX_SAFE_INTEGER;
 let cheapestElem = undefined;
-let MAX_ALLOWED_HIGHLIGHT_ITEMS = 4
+let MAX_ALLOWED_HIGHLIGHT_ITEMS = 5
 
 unitPrices
   .map(e => ({ price: getPriceFor(e), e }))
   .filter(e => e.price !== undefined)
   .sort((a, b) => a.price - b.price)
-  .forEach((e, i) => i <= MAX_ALLOWED_HIGHLIGHT_ITEMS ? highlightElement(e.e, i) : "")
+  .forEach((e, i) => i + 1 <= MAX_ALLOWED_HIGHLIGHT_ITEMS ? highlightElement(e.e, i) : "")
 
 
 
 function getPriceFor(e) {
   const unitPrice = e.innerText
-  const perKgOrLOrPiece = unitPrice.match(/kr ([0-9,]*) per (kg|l|piece|stk)/);
+  const perKgOrLOrPiece = unitPrice.match(/kr ([0-9,]*) per (kg|l|piece|stk|m)/);
   if (!perKgOrLOrPiece) {
     // something different --> send to backend server to improve
     // console.log(unitPrice, document.location)
